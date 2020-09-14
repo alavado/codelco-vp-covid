b590d24282749f3381ab5c65a52004e4c99c3442
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-09-14 16:26:21 -0300

    pone datos en grafico de toda codelco

--- src/components/Graficos/Graficos.js
+++ src/components/Graficos/Graficos.js
@@ -1,25 +1,76 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import divisiones from '../../data/csv/data_codelco.json'
 import './Graficos.css'
-import { Bar } from 'react-chartjs-2'
+import { Chart, Bar } from 'react-chartjs-2'
 import moment from 'moment'
+import 'moment/locale/es'
+
+Chart.defaults.global.defaultFontFamily = 'Montserrat'
 
 const Graficos = () => {
 
-  const [labels, data] = useMemo(() => {
-    const datosCodelco = divisiones.find(d => d.codigo === 'Codelco')
+  const [acumulados, setAcumulados] = useState(false)
+
+  const [fechas, serie, total] = useMemo(() => {
+    const serie = divisiones.series.find(d => d.codigo === 'Codelco').propios.slice()
     return [
-      datosCodelco.map(d => d.fecha),
-      datosCodelco.map(d => d.casosNuevos)
+      divisiones.fechas,
+      acumulados
+        ? serie.reduce((prev, v) => [...prev, prev.slice(-1)[0] + v], [0]).slice(1)
+        : serie,
+      serie.reduce((sum, v) => sum + v)
     ]
-  }, [])
+  }, [acumulados])
 
   return (
     <div className="Graficos">
-      {/* <Bar
-        labels
-        data
-      /> */}
+      <button onClick={() => setAcumulados(!acumulados)}>toggle</button>
+      <div className="Graficos__contenedor_grafico">
+        <div className="Grafico__titulo">
+          <h2>Toda CODELCO</h2>
+          <p className="Grafico__total_acumulado">{total}</p>
+          <p>Casos confirmados</p>
+        </div>
+        <div className="Graficos__grafico">
+          <Bar
+            data={{
+              labels: fechas,
+              datasets: [
+                {
+                  data: serie,
+                  backgroundColor: '#52859E',
+                }
+              ]
+            }}
+            options={{
+              maintainAspectRatio: false,
+              legend: {
+                display: false
+              },
+              scales: {
+                xAxes: [{
+                  ticks: {
+                    maxRotation: 0,
+                    autoSkip: false,
+                    callback: v => {
+                      const fecha = moment(v)
+                      return fecha.date() === 1 ? fecha.format('D MMM') : null
+                    }
+                  },
+                  gridLines: {
+                    display: false
+                  }
+                }],
+                yAxes: [{
+                  gridLines: {
+                    display: false
+                  }
+                }]
+              }
+            }}
+          />
+        </div>
+      </div>
     </div>
   )
 }
