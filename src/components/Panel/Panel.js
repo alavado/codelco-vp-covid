11aff231ced91921715dd599ef74840e555eed1e
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-10-15 21:33:00 -0300

    agrega hexagonos

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import divisiones from '../../data/csv/divisiones.json'
 import './Panel.css'
 
 const indicadores = [
@@ -19,17 +20,38 @@ const indicadores = [
   }
 ]
 
+const hexagonosFalsos = [0, 3, 7]
+
 const Panel = () => {
   return (
     <div className="Panel">
+      <div className="Panel__encabezados">
+        <div>Dimensión</div>
+        <div className="Panel__encabezado_resultados">Resultados</div>
+      </div>
       {indicadores.map(({ nombre }) => (
         <div className="Panel__contenedor_indicador">
           <div className="Panel__nombre_indicador">
             {nombre}
           </div>
           <div className="Panel__indicador_global">
+            <div className="Panel__hexagono">
+              Global
+            </div>
           </div>
           <div className="Panel__indicadores_divisiones">
+            <div className="Panel__indicadores_contenedor_hexagonos">
+              {divisiones.filter(d => d.codigo !== 'Codelco').map((division, i) => (
+                <>
+                  {hexagonosFalsos.includes(i) &&
+                    <div className="Panel__hexagono_pequeño Panel__hexagono_pequeño--relleno" />
+                  }
+                  <div className="Panel__hexagono_pequeño">
+                    {division.codigoCorto}
+                  </div>
+                </>
+              ))}
+            </div>
           </div>
         </div>
       ))}
