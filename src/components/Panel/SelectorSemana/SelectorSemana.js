586bfe365283585cd7c5a0282a15adb85ef8d20f
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-12-09 00:45:38 -0300

    sticky semana

--- src/components/Panel/SelectorSemana/SelectorSemana.js
+++ src/components/Panel/SelectorSemana/SelectorSemana.js
@@ -32,7 +32,7 @@ const SelectorSemana = () => {
           <InlineIcon className="SelectorSemana__icono_boton" icon={iconoAtras} />
         </button>
         <div className="SelectorSemana__semana_seleccionada">
-          <div>Semana {ultimaSemana + retroceso}</div>
+          <div>Semana {ultimaSemana + retroceso} / 2020</div>
           <div className="SelectorSemana__fechas_semana">
             {semanasEpidemiologicas[ultimaSemana + retroceso].texto}
           </div>
